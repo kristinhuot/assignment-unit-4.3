754de3a6fbc72e03c6f4299de3de805f904d6cf0
Kristin Huot <kristin.huot@gmail.com>
2024-03-14 11:15:45 -0500

    initial commit

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,14 +1,37 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket= []; 
 
+function addItem (item){
+    basket.unshift(item); 
+    return true 
+} 
+console.log('Adding an item to the basket', addItem('Apples'));
+console.log('The basket now has:', basket);
 
+/////////////////////////////////
 
+basket=['bread', 'milk', 'cheese']
 
+function listItems () {
+    for (let i = 0; i < basket.length; i++) {
+        console.log('This is an item in the basket:', basket [i]); 
+    }
+}
+listItems();
 
+/////////////////////////////////
 
+function empty (){
+    while (basket.length > 0) {
+        basket.pop();
+    }
+}
 
+console.log('This is now my basket:', empty());
 
+/////////////////////////////////
 
 
 
